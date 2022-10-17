7409d36872517b92b3e09902a6b4b195e07fd54c
leo_songwei <leo_songwei@outlook.com>
2022-10-17 17:27:48 +1100

    shader

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,58 @@
+extern crate sdl2;
+extern crate gl;
+
+extern crate log;
+
+mod shader;
+
+use sdl2::event::Event;
+use sdl2::keyboard::Keycode;
+use sdl2::video::GLProfile;
+
+use simple_logger::SimpleLogger;
+
 fn main() {
-    println!("Hello, world!");
+    SimpleLogger::new().with_colors(true).init().unwrap();
+    log::info!("start.");
+
+    let sdl_context = sdl2::init().unwrap();
+    let video_subsystem = sdl_context.video().unwrap();
+    
+    let gl_attr = video_subsystem.gl_attr();
+    gl_attr.set_context_profile(GLProfile::Core);
+    gl_attr.set_context_version(4, 5);
+
+    let window = video_subsystem.window("Window", 800, 600)
+        .opengl()
+        .build()
+        .unwrap();
+
+    // Unlike the other example above, nobody created a context for your window, so you need to create one.
+    let _ctx = window.gl_create_context().unwrap();
+    gl::load_with(|name| video_subsystem.gl_get_proc_address(name) as *const _);
+    
+    debug_assert_eq!(gl_attr.context_profile(), GLProfile::Core);
+    debug_assert_eq!(gl_attr.context_version(), (4, 5));
+
+    let mut event_pump = sdl_context.event_pump().unwrap();
+
+    'running: loop {
+        unsafe {
+            let (window_width, window_height) = window.size();
+            gl::Viewport(0, 0, window_width as i32, window_height as i32);
+            gl::ClearColor(0.1, 0.1, 0.1, 1.0);
+            gl::Clear(gl::COLOR_BUFFER_BIT);
+        }
+
+        window.gl_swap_window();
+        for event in event_pump.poll_iter() {
+            match event {
+                Event::Quit {..} | Event::KeyDown { keycode: Some(Keycode::Escape), .. } => {
+                    break 'running
+                },
+                _ => {}
+            }
+        }
+        ::std::thread::sleep(::std::time::Duration::new(0, 1_000_000_000u32 / 60));
+    }
 }
---
+++ src/shader.rs
@@ -0,0 +1,129 @@
+use gl;
+use std;
+use std::ffi::{CStr, CString};
+
+pub struct Program {
+    id: gl::types::GLuint,
+}
+
+impl std::ops::Drop for Program {
+    fn drop(&mut self) {
+        log::debug!("droping program");
+        unsafe {
+            gl::DeleteProgram(self.id);
+        }
+    }
+}
+
+impl Program {
+    pub fn from_shader_strings(vs: &str, fs: &str) -> Result<Program, String> {
+        let program_id = unsafe { gl::CreateProgram() };
+
+        let vs = match shader_from_source(vs, gl::VERTEX_SHADER) {
+            Ok(id) => id,
+            Err(message) => return Err(message)
+        };
+        let fs = match shader_from_source(fs, gl::FRAGMENT_SHADER) {
+            Ok(id) => id,
+            Err(message) => return Err(message)
+        };
+
+        unsafe {
+            gl::AttachShader(program_id, vs.id);
+            gl::AttachShader(program_id, fs.id);
+            gl::LinkProgram(program_id);
+        }
+
+        let mut success: gl::types::GLint = 1;
+        unsafe {
+            gl::GetProgramiv(program_id, gl::LINK_STATUS, &mut success);
+        }
+
+        if success == 0 {
+            let mut len: gl::types::GLint = 0;
+            unsafe {
+                gl::GetProgramiv(program_id, gl::INFO_LOG_LENGTH, &mut len);
+            }
+
+            let error = create_whitespace_cstring_with_len(len as usize);
+
+            unsafe {
+                gl::GetProgramInfoLog(
+                    program_id,
+                    len,
+                    std::ptr::null_mut(),
+                    error.as_ptr() as *mut gl::types::GLchar,
+                );
+            }
+
+            unsafe { gl::DeleteProgram(program_id); }
+
+            return Err(error.to_string_lossy().into_owned());
+        }
+
+        unsafe {
+            gl::DetachShader(program_id, vs.id);
+            gl::DetachShader(program_id, fs.id);
+        }
+
+        Ok(Program { id: program_id })
+    }
+}
+
+pub struct Shader {
+    id: gl::types::GLuint,
+}
+
+impl std::ops::Drop for Shader {
+    fn drop(&mut self) {
+        log::debug!("droping shader");
+        unsafe {
+            gl::DeleteShader(self.id)
+        }
+    }
+}
+
+fn shader_from_source(source: &str, kind: gl::types::GLenum) -> Result<Shader, String> {
+    log::debug!("Compile shader");
+
+    let c_source = unsafe { CStr::from_ptr(source.as_ptr() as *const i8)};
+    let id = unsafe {gl::CreateShader(kind)};
+    unsafe {
+        gl::ShaderSource(id, 1, &c_source.as_ptr(), std::ptr::null());
+        gl::CompileShader(id);
+    }
+    let mut success: gl::types::GLint = 1;
+    unsafe {
+        gl::GetShaderiv(id, gl::COMPILE_STATUS, &mut success);
+    }
+    if success == 0 {
+        let mut len: gl::types::GLint = 0;
+        unsafe {
+            gl::GetShaderiv(id, gl::INFO_LOG_LENGTH, &mut len);
+        }
+
+        let error = create_whitespace_cstring_with_len(len as usize);
+
+        unsafe {
+            gl::GetShaderInfoLog(
+                id,
+                len,
+                std::ptr::null_mut(),
+                error.as_ptr() as *mut gl::types::GLchar,
+            );
+        }
+
+        return Err(error.to_string_lossy().into_owned());
+    }
+
+    Ok(Shader { id })
+}
+
+fn create_whitespace_cstring_with_len(len: usize) -> CString {
+    // allocate buffer of correct size
+    let mut buffer: Vec<u8> = Vec::with_capacity(len + 1);
+    // fill it with len spaces
+    buffer.extend([b' '].iter().cycle().take(len));
+    // convert buffer to CString
+    unsafe { CString::from_vec_unchecked(buffer) }
+}
